5d1a0b994a04f28dcea536144ac305bfbe45e8af
Mokwa Moffat <moffatmkwa12@gmail.com>
2025-05-31 22:35:56 +0300

    feat: preload HTML templates and integrate dynamic product rendering
    - Loaded overview, card, and product templates from disk
    - Enabled dynamic injection of product data into templates
    - Set up template system foundation for overview and product pages

---
+++ index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+const slugify = require('slugify');
+const replaceTemplate = require('./modules/replaceTemplate');
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
