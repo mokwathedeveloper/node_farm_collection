5d77a428bb7885b3b4f400a2347b237e398e4ff0
Mokwa Moffat <moffatmkwa12@gmail.com>
2025-06-02 15:23:25 +0300

    Create Home component with welcome message

---
+++ client/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Farm Collection</h1>
+      <p>Browse our selection of fresh, organic farm products.</p>
+    </div>
+  );
+};
+
+export default Home;
