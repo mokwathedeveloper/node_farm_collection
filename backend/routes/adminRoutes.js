51d471469078a46e1c6ef681c9b7e5cd966b30f9
Mokwa Moffat <moffatmokwa12@gmail.com>
2025-06-15 13:57:45 +0300

    Clean master branch - remove all files with errors

--- backend/routes/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const adminController = require('../controllers/adminController');
-
-const router = express.Router();
-
-router.get('/add-product', adminController.getAddProductForm);
-router.post('/add-product', adminController.addProduct);
-
-module.exports = router;
--- client/src/components/Footer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
-
-const Footer = () => {
-  // Component implementation...
-};
-
-export default Footer;
